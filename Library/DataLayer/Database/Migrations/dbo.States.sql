d7765a905d77daac1453b6c55b3c1a811f5ff47c
Oskarowski <oscar.kacprzak@gmail.com>
2024-05-27 20:38:43 +0200

    fix FK on Statues

--- Library/DataLayer/Database/Migrations/dbo.States.sql
+++ Library/DataLayer/Database/Migrations/dbo.States.sql
@@ -4,5 +4,5 @@
     [productGuid] VARCHAR(36) NOT NULL, 
     [quantity] INT NOT NULL, 
     [LastUpdatedDate] DATE NOT NULL, 
-    CONSTRAINT [FK_States_ToProductTable] FOREIGN KEY ([guid]) REFERENCES [Books]([guid])
+    CONSTRAINT [FK_States_ToProductTable] FOREIGN KEY (productGuid) REFERENCES [Books]([guid])
 )
